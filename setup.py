02abfd95c3194cbfe6c7f339bf56839e134eac5e
dan <dansebcar@gmail.com>
2021-10-08 22:21:29 +0200

    Explicit dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="redirect",
     version="0.1",
-    packages=setuptools.find_packages(),
+    install_requires=["aiohttp"],
     entry_points={"console_scripts": ["redirect=redirect:main"]},
+    packages=setuptools.find_packages(),
 )
